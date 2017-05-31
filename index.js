5bf82a2ef72a948a5d63112b31fae032c9fc79d5
Omer Dobzinsky <omerdob@gmail.com>
2017-05-31 18:09:31 +0300

    no message

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
+const repl = require('repl');
+const vm = require('vm');
+const net = require('net');
+
 const dsmCalendarApp = require('./source/dsm-calendar.app');
+const logger = require('./source/utils/logger');
 
-let port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+const replPort = port + 1;
 
-dsmCalendarApp.listen(port, () => console.log(`server started on port ${port}`));
+const initializeReplServerContext = replServer => {
+    replServer.context = vm.createContext({
+        app: dsmCalendarApp
+    });
+};
+
+net
+    .createServer(socket => {
+        let replServer = repl
+            .start({
+                input: socket,
+                output: socket
+            })
+            .on('reset', () => initializeReplServerContext(replServer))
+            .on('exit', () => {
+                logger.info(`Terminal ended from ${socket.remoteAddress}`);
+                socket.end();
+            });
+        
+        initializeReplServerContext(replServer);
+        logger.info(`New terminal opened from ${socket.remoteAddress}`);
+    })
+    .listen(replPort);
+
+dsmCalendarApp.listen(port, () => logger.info(`Server started on port ${port}`));
--- source/utils/dsm-db-connection.js
+++ source/utils/dsm-db-connection.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
+const logger = require('./logger');
+
 mongoose.Promise = require('bluebird');
 
 let dsmDbConnection = mongoose.createConnection(config.get('connectionString'));
+
+dsmDbConnection.on('error', err => logger.error(err));
 
 process
     .on('SIGINT', dsmDbConnection.close)
